6dff7685c2828d6c1c21bf05367a64990c1b6d94
Aditya <lapratomo24@yahoo.com>
2024-11-20 21:28:52 +0100

    loaded env variable; fetched api key

--- main.py
+++ main.py
@@ -3,5 +3,19 @@ import os
 import sys
 import openai
 
+load_dotenv()
+
 def main():
-    pass
+    '''parse environment variable'''
+    api_key = os.getenv("OPENAI_API_KEY")
+    if not api_key:
+        print("Missing API Key. Program aborted.")
+        sys.exit(1)
+    
+    if len(sys.argv) < 2:
+        print("Usage: Type in 'python chatbot.py <chatbot_name>'")
+        sys.argv(1)
+        
+    chatbot_name = sys.argv[1]
+    openai.api_key = api_key
+
